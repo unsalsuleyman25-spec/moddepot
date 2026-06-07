04bde0d937b8da2f76c99edda95f79cb8b5b0d29
unsalsuleyman25-spec <unsalsuleyman25@gmail.com>
2026-06-07 09:12:43 +0300

    ModDepot ilk site dosyaları

---
+++ js/mods.js
@@ -0,0 +1,20 @@
+// ModDepot mod listesi
+// Yeni modlar buradan yönetilir.
+// İndirme linki yoksa "download": "#" kalır ve sitede Yakında görünür.
+// Gerçek link eklenirse İndir butonu yeni sekmede açılır.
+
+window.ModDepotMods = [
+  {
+    "id": "basak-traktor-paketi",
+    "name": "Başak Traktör Paketi",
+    "category": "Traktör",
+    "description": "FS25 için kırmızı Başak traktör modu. Çiftlik işleri için sade, kullanışlı ve güçlü traktör paketi.",
+    "version": "1.0",
+    "size": "120 MB",
+    "author": "ModDepot",
+    "addedDate": "06.06.2026",
+    "image": "images/basak-traktor.png",
+    "icon": "🚜",
+    "download": "https://ay.live/basak"
+  }
+];
